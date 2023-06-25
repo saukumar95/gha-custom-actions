c645013ce5d985f1af78865bb9376b46eb7306d4
Saurabh Kumar <saukumar95@gmail.com>
2023-06-25 09:50:33 +0530

    Updated custom action with move operation

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -2,11 +2,8 @@ import { notice, getInput, setOutput } from '@actions/core'
 import exec from '@actions/exec'
 
 const run = async () => {
-    notice('Hello world! This is my first custom JS action')
-    const a = typeof getInput('a', {required: true}) === 'string' ? Number(getInput('a', {required: true})) : getInput('a', {required: true})
-    const b = typeof getInput('b', {required: true}) === 'string' ? Number(getInput('b', {required: true})) : getInput('b', {required: true})
-    const sum = a + b;
-    setOutput('sum', sum)
-    notice('First custom action execution done..')
+    const sourcePath = getInput('source-path', {required: true});
+    const destinationPath = getInput('destination-path', {required: true})
+    notice(`Inputs ${sourcePath} & ${destinationPath}`)
 }
 run()
