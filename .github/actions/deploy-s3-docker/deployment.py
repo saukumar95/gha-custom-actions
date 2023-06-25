56ac30ae524d5f57aad3a6a21a61b8065d294e10
Saurabh Kumar <saukumar95@gmail.com>
2023-06-25 18:57:39 +0530

    Added log

--- .github/actions/deploy-s3-docker/deployment.py
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -3,7 +3,7 @@ from os import environ, rename
 def run():
     source_path = environ['INPUT_SOURCE-PATH']
     destination_path = environ['INPUT_DESTINATION-PATH']
-    rename(source_path, destination_path)
+    # rename(source_path, destination_path)
     print(f"Inputs {source_path}, {destination_path}")
 
 if __name__ == '__main__':
