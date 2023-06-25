f38b6826895b095883d3fbcc729428f5975d5dcc
Saurabh Kumar <saukumar95@gmail.com>
2023-06-25 10:03:13 +0530

    Removed unused async keyword

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -1,7 +1,7 @@
 import { notice, getInput, setOutput } from '@actions/core'
 import { getExecOutput } from '@actions/exec'
 
-const run = async () => {
+const run = () => {
     const sourcePath = getInput('source-path', { required: true });
     const destinationPath = getInput('destination-path', { required: true })
     getExecOutput(`mv ${sourcePath} ${destinationPath}`)
