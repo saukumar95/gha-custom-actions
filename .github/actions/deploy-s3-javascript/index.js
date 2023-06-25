0c9083497f51f5043007f1f3ca9b886b5fd76f95
Saurabh Kumar <saukumar95@gmail.com>
2023-06-25 09:36:13 +0530

    Added logic in run function for first custom GHA

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -1,7 +1,12 @@
-import { notice } from '@actions/core'
+import { notice, getInput, setOutput } from '@actions/core'
 import exec from '@actions/exec'
 
 const run = async () => {
     notice('Hello world! This is my first custom JS action')
+    const a = getInput('a', {required: true})
+    const b = getInput('b', {required: true})
+    const sum = a + b;
+    setOutput('sum', sum)
+    notice('First custom action execution done..')
 }
 run()
