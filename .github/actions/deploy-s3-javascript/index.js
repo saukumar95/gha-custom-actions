5ae7048062004aa0355eacb1dff5d589242bd194
Saurabh Kumar <saukumar95@gmail.com>
2023-06-25 09:43:09 +0530

    Code correction for second input

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -4,7 +4,7 @@ import exec from '@actions/exec'
 const run = async () => {
     notice('Hello world! This is my first custom JS action')
     const a = typeof getInput('a', {required: true}) === 'string' ? Number(getInput('a', {required: true})) : getInput('a', {required: true})
-    const b = typeof Number(getInput('b', {required: true})) === 'string' ? Number(getInput('b', {required: true})) : getInput('b', {required: true})
+    const b = typeof getInput('b', {required: true}) === 'string' ? Number(getInput('b', {required: true})) : getInput('b', {required: true})
     const sum = a + b;
     setOutput('sum', sum)
     notice('First custom action execution done..')
