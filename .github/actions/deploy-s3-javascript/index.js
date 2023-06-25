e54c272a33bf2d06671cdf3389f59d9ad91d767f
Saurabh Kumar <saukumar95@gmail.com>
2023-06-25 10:00:13 +0530

    Added code for move operation

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -1,9 +1,10 @@
 import { notice, getInput, setOutput } from '@actions/core'
-import exec from '@actions/exec'
+import { getExecOutput } from '@actions/exec'
 
 const run = async () => {
-    const sourcePath = getInput('source-path', {required: true});
-    const destinationPath = getInput('destination-path', {required: true})
+    const sourcePath = getInput('source-path', { required: true });
+    const destinationPath = getInput('destination-path', { required: true })
+    getExecOutput(`mv ${sourcePath} ${destinationPath}`)
     notice(`Inputs ${sourcePath} & ${destinationPath}`)
 }
 run()
