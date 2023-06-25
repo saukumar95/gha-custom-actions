c867264bf77dad15bc6ce1630d4cff9738b0735c
Saurabh Kumar <saukumar95@gmail.com>
2023-06-25 09:41:53 +0530

    Handled for input type string and number

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -3,8 +3,8 @@ import exec from '@actions/exec'
 
 const run = async () => {
     notice('Hello world! This is my first custom JS action')
-    const a = getInput('a', {required: true})
-    const b = getInput('b', {required: true})
+    const a = typeof getInput('a', {required: true}) === 'string' ? Number(getInput('a', {required: true})) : getInput('a', {required: true})
+    const b = typeof Number(getInput('b', {required: true})) === 'string' ? Number(getInput('b', {required: true})) : getInput('b', {required: true})
     const sum = a + b;
     setOutput('sum', sum)
     notice('First custom action execution done..')
