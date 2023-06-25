f439d02b04d9a2dbc8cf4089cbda9f99f6e7c1dc
Saurabh Kumar <saukumar95@gmail.com>
2023-06-25 09:28:08 +0530

    Converted imports to ES module import

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -1,7 +1,7 @@
-import core from '@actions/core'
+import { notice } from '@actions/core'
 import exec from '@actions/exec'
 
 const run = async () => {
-    core.notice('Hello world! This is my first custom JS action')
+    notice('Hello world! This is my first custom JS action')
 }
 run()
